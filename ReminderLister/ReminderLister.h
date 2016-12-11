6cc0e5a14b531aa34eb00212a8b1c614c62223bf
Salieu Kamara <kamaras@flg05-mac-12.coventry.ac.uk>
2016-12-11 20:55:42 +0000

    Add project files

---
+++ ReminderLister/ReminderLister.h
@@ -0,0 +1,19 @@
+//
+//  ReminderLister.h
+//  ReminderLister
+//
+//  Created by Salieu Kamara on 11/12/2016.
+//  Copyright © 2016 Salieu Kamara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReminderLister.
+FOUNDATION_EXPORT double ReminderListerVersionNumber;
+
+//! Project version string for ReminderLister.
+FOUNDATION_EXPORT const unsigned char ReminderListerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReminderLister/PublicHeader.h>
+
+
